282c094db8ca95f9f6a252c41b2076f2b7c303f2
muatheJamil <muathe@trigger-apps.com>
2022-04-13 18:29:08 +0300

    first commit

---
+++ SuperHeroApi/Controllers/SuperHeroController.cs
@@ -0,0 +1,25 @@
+﻿using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+
+namespace SuperHeroApi.Controllers
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class SuperHeroController : ControllerBase
+    {
+        [HttpGet]
+        public async Task<ActionResult<List<SuperHero>>> Get()
+        {
+            var heros = new List<SuperHero>
+            { new SuperHero {
+                Id = 1,
+                Name = "Spider Man",
+                FirstName = "Peter",
+                SecondName = "Tamer",
+                Place = "New York City"}
+            };
+
+            return Ok(heros);
+        }
+    }
+}
